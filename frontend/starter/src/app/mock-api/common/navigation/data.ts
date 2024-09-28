ae95b7f79a809347f13b04d8c937b431e223179e
Martiinfierro <mfdp2@gcloud.ua.es>
2024-09-28 13:45:06 +0200

    Preparación del entorno

---
+++ frontend/starter/src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,46 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'grafo',
+        title: 'grafo',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/grafo'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'grafo',
+        title: 'grafo',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/grafo'
+    },
+    {
+        id   : 'lista',
+        title: 'lista',
+        type : 'basic',
+        icon : 'heroicons_outline:list-bullet',
+        link : '/lista'
+    }
+];
