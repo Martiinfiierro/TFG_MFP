a879442448584de2b6275f5d08d541f52e0d7bdd
Martiinfierro <mfdp2@gcloud.ua.es>
2024-09-28 11:10:06 +0200

    Primer commit

---
+++ backend/v2/coordinator/runm.bat
@@ -0,0 +1,2 @@
+cls
+nodemon ./coordinator 3000 http://127.0.001:3100 http://127.0.001:3101 main
---
+++ backend/v2/coordinator/runs.bat
@@ -0,0 +1,2 @@
+cls
+nodemon ./coordinator 3001 http://127.0.001:3100 http://127.0.001:3101 subs http://127.0.0.1:3000
