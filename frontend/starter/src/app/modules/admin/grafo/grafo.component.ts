8640457250b72366a50b6bbbee1210d5dc1c821b
Martiinfierro <mfdp2@gcloud.ua.es>
2024-10-26 11:47:08 +0200

    conexiones grafos a falta de cambiatr los subs por los main en caso de fallo

--- frontend/starter/src/app/modules/admin/grafo/grafo.component.ts
+++ frontend/starter/src/app/modules/admin/grafo/grafo.component.ts
@@ -91,7 +91,35 @@ export class GrafoComponent{
                                     tipo_nodo, 
                                     id: nodo.id 
                                 };
-                            })
+                            }),
+
+                            links: res.reduce((acc: any[], item2: any) => {
+                                const { nodo, status } = item2;
+                    
+                                // Enlaces de Controladores a Procesadores
+                                if (nodo.tipo_nodo === 'Controlador' && status === true && nodo.nombre === 'main') {
+                                    res.forEach((nodoTarget: any) => {
+                                        if (nodoTarget.nodo.tipo_nodo === "Procesador" && nodoTarget.status === true) {
+                                            acc.push({
+                                                source: String(nodo.id),
+                                                target: String(nodoTarget.nodo.id)
+                                            });
+                                        }
+                                    });
+                                }
+                    
+                                // Enlace de Balanceadores y Controladores "subs" hacia "main"
+                                if ((nodo.tipo_nodo === 'Balanceador' || nodo.tipo_nodo === 'Controlador') && nodo.nombre === 'subs' && status === true) {
+                                    const nodoTarget = res.find((n: any) => n.nodo.tipo_nodo === nodo.tipo_nodo && n.nodo.nombre === 'main' && n.status === true);
+                                    if (nodoTarget) {
+                                        acc.push({
+                                            source: String(nodo.id),
+                                            target: String(nodoTarget.nodo.id)
+                                        });
+                                    }
+                                }
+                                return acc;
+                            }, [])
                         }
                     ]
                 });
@@ -99,9 +127,9 @@ export class GrafoComponent{
             });
         });
     } 
-
+    
     initChart(): void {
-        this.http.getNodos().subscribe((res) =>{
+        this.http.getNodos().subscribe((res) => {
             const requests = res.nodos.map((nodo: any) => 
                 this.http.readDebug(nodo.url).pipe(
                     map((val: any) => ({
@@ -116,11 +144,73 @@ export class GrafoComponent{
                     }))
                 )
             );
-            forkJoin(requests).subscribe((data: any) => {
-                console.log(data); 
+            forkJoin(requests).subscribe((res: any) => {
                 const chartDom = document.getElementById('grafo');
                 this.myChart = echarts.init(chartDom!);
+    
+                // Procesa los nodos para `data`
+                const data = res.map((item: any) => {
+                    const { nodo, status } = item;
+                    const { tipo_nodo } = nodo;
+                    const symbol = tipo_nodo === 'Balanceador' ? 'square' : tipo_nodo === 'Controlador' ? 'circle' : 'diamond';
+                    const size = tipo_nodo === 'Procesador' ? 35 : 50;
+                    let color = tipo_nodo === 'Balanceador' ? '#1E90FF' : tipo_nodo === 'Controlador' ? '#FFA500' : '#A75DB3';
+                    if (!status) {
+                        color = this.grey(color);
+                    }
+    
+                    return { 
+                        id: nodo.id,
+                        name: nodo.nombre, 
+                        symbolSize: size, 
+                        symbol, 
+                        itemStyle: { color }, 
+                        tipo_nodo
+                    };
+                });
+    
+                // Construye enlaces `links`
+                const links = res.reduce((acc: any[], item2: any) => {
+                    const { nodo, status } = item2;
         
+                    // Enlaces de Controladores a Procesadores
+                    if (nodo.tipo_nodo === 'Controlador' && status === true && nodo.nombre === 'main') {
+                        res.forEach((nodoTarget: any) => {
+                            if (nodoTarget.nodo.tipo_nodo === "Procesador" && nodoTarget.status === true) {
+                                acc.push({
+                                    source: nodo.id,
+                                    target: nodoTarget.nodo.id
+                                });
+                            }
+                        });
+                    }
+                    else if (nodo.tipo_nodo === 'Controlador' && status === false && nodo.nombre === 'main') {
+                        if(nodo.tipo_nodo === 'Controlador' && status === true && nodo.nombre === 'subs'){
+                            res.forEach((nodoTarget: any) => {
+                                if (nodoTarget.nodo.tipo_nodo === "Procesador" && nodoTarget.status === true) {
+                                    acc.push({
+                                        source: nodo.id,
+                                        target: nodoTarget.nodo.id
+                                    });
+                                }
+                            });
+                        }
+                    }
+        
+                    // Enlace de Balanceadores y Controladores "subs" hacia "main"
+                    if ((nodo.tipo_nodo === 'Balanceador' || nodo.tipo_nodo === 'Controlador') && nodo.nombre === 'subs' && status === true) {
+                        const nodoTarget = res.find((n: any) => n.nodo.tipo_nodo === nodo.tipo_nodo && n.nodo.nombre === 'main' && n.status === true);
+                        if (nodoTarget) {
+                            acc.push({
+                                source: nodo.id,
+                                target: nodoTarget.nodo.id
+                            });
+                        }
+                    }
+                    return acc;
+                }, []);
+    
+                // Configuración de opciones de ECharts
                 const option = {
                     tooltip: {},
                     series: [
@@ -140,52 +230,41 @@ export class GrafoComponent{
                                 show: true,
                                 position: 'inside'
                             },
-                            data: data.map((res: any) => {
-                                let { nodo, status } = res;
-                                let { tipo_nodo } = nodo;
-                                let symbol = tipo_nodo === 'Balanceador' ? 'square' : tipo_nodo === 'Controlador' ? 'circle' : 'diamond';
-                                let size = tipo_nodo === 'Procesador' ? 35 : 50;
-                                let color = tipo_nodo === 'Balanceador' ? '#1E90FF' : tipo_nodo === 'Controlador' ? '#FFA500' : '#A75DB3'
-                                if(!status){
-                                    color = this.grey(color);
-                                }
-        
-                                return { 
-                                    name: nodo.nombre, 
-                                    symbolSize: size, 
-                                    symbol, 
-                                    itemStyle: { 
-                                        color 
-                                    }, 
-                                    tipo_nodo, 
-                                    id: nodo.id 
-                                };
-                            }),
+                            data: data,
+                            links: links.map((link: any) => ({
+                                source: String(link.source),
+                                target: String(link.target),
+                            })),
                             lineStyle: {
-                                color: 'source',
-                                curveness: 0.7
-                            }
+                                color: '#aaa',
+                                width: 2,
+                                opacity: 0.9,
+                                curveness: 0.3
+                            },
+                            edgeSymbol: ['none', 'arrow'],
+                            edgeSymbolSize: [4, 10], 
                         }
                     ]
                 };
-            
+                
                 // Establece las opciones del gráfico
                 this.myChart.setOption(option);
-            
+    
                 // Manejar clic en los nodos
-                this.myChart.on('click', (params) => {
+                this.myChart.on('click', (params: any) => {
                     if (params.dataType === 'node') {
                         const nodoId = (params.data as any).id;
                         const tipoNodo = (params.data as any).tipo_nodo;
-        
+    
                         this.dialogo(tipoNodo, nodoId);
                     }
                 });
-
+    
                 this.updateGrafo();
             });
         });
     }
+    
 
     grey(hexColor: any) {
         // Convertir el color hexadecimal a RGB
